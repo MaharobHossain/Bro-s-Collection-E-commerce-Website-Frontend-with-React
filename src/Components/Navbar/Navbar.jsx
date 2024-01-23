ba6e2da1edcfa19781b4150a8979d807e413a88e
maharob888 <maharobhossain8880@gmail.com>
2024-01-23 22:08:08 +0600

    Molile Menu Add

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
 
         <Link to='/cart'><img src={cart} alt="" /> </Link>
         <div className="nav-cart-count">{getTotalCartItems()}</div>
-        <Link to='/login'><button>Login</button></Link>
+        <Link to='/login'><button>Profile</button></Link>
 
       </div>
 
